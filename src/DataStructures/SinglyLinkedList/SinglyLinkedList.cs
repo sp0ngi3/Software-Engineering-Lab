b0322704edf2354da5a68189e2b94242944ee4c5
sp0ngi3 <studnia12321@yahoo.com>
2026-07-19 19:12:17 +0200

    feat: Added a crude Singly Linked List Class

---
+++ src/DataStructures/SinglyLinkedList/SinglyLinkedList.cs
@@ -0,0 +1,29 @@
+﻿namespace DataStructures.SinglyLinkedList
+{
+    /// <summary>
+    /// Represents a singly linked list data structure.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the singly linked list.</typeparam>
+    public class SinglyLinkedList<T>
+    {
+        /// <summary>
+        /// Reference to the start of the list.
+        /// </summary>
+        private SinglyLinkedListNode<T>? _head;
+
+        /// <summary>
+        /// Reference to the end of the list.
+        /// </summary>
+        private SinglyLinkedListNode<T>? _tail;
+
+        /// <summary>
+        /// Creates a new instance of the SinglyLinkedList class with an initial value.
+        /// </summary>
+        /// <param name="value">The initial value to be stored in the list.</param>
+        public SinglyLinkedList(T value)
+        {
+            _head = new SinglyLinkedListNode<T>(value);
+            _tail = _head;
+        }
+    }
+}
---
+++ src/DataStructures/SinglyLinkedList/SinglyLinkedListNode.cs
@@ -0,0 +1,20 @@
+﻿namespace DataStructures.SinglyLinkedList
+{
+    /// <summary>
+    /// Represents a node in a singly linked list.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the singly linked list.</typeparam>
+    public class SinglyLinkedListNode<T>
+    {
+        public T Value { get; set; }
+        public SinglyLinkedListNode<T>? Next { get; set; }
+        public SinglyLinkedListNode(T value)
+        {
+            Value = value;
+            Next = null;
+        }
+    }
+
+    // A singly linked list node consists of two things: Data and the pointer to the next node.
+    // The last node in the singly linked list points to null.
+}
